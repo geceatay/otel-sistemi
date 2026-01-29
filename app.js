f979dbf500f3829999ef9a824798f50a29861e74
geceatay <ilkin_mvh@mail.ru>
2026-01-29 23:32:13 +0400

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-a
+function login() {
+  const user = document.getElementById("username").value;
+  const pass = document.getElementById("password").value;
+
+  // ŞİMDİLİK SAHTE
+  if (user === "reception" && pass === "1234") {
+    window.location.href = "dashboard.html";
+  } else if (user === "admin" && pass === "admin") {
+    window.location.href = "admin.html";
+  } else {
+    alert("Hatalı giriş");
+  }
+}
